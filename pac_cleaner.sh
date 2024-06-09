6856e75b53902fa2dbb8fde31b49a7dbad64e39d
Benjamin Duguay <153779837+duguayworld@users.noreply.github.com>
2024-06-08 20:10:02 -0400

    Update pac_cleaner.sh

--- pac_cleaner.sh
+++ pac_cleaner.sh
@@ -1,30 +1,60 @@
 #!/bin/bash
 
-# Fetch the mirrorlist using reflector for Canada and the USA and select the best mirrors
-echo "Fetching mirror list for Canada and the USA..."
-MIRROR_LIST=$(reflector --country CA,US --latest 20 --sort rate --save /etc/pacman.d/mirrorlist)
+# Fetch the mirrorlist 
+echo "Fetching mirror list..."
+echo "$1" | eos-rankmirrors
+
+read -p "Press Y to continue, or N to exit: " input
+if [[ ! $input =~ ^[Yy]$ ]]; then
+    echo "Exiting..."
+    exit 0
+fi
 
 # Synchronize package databases and update packages
 echo "Synchronizing package databases and updating packages..."
-yay -Syyu
+echo "$1" | yay -Syyu
+
+read -p "Press Y to continue, or N to exit: " input
+if [[ ! $input =~ ^[Yy]$ ]]; then
+    echo "Exiting..."
+    exit 0
+fi
 
 # Clean the package cache
 echo "Cleaning package cache..."
-sudo pacman -Sc
+echo "$1" | sudo pacman -Sc
 
-# Vacuum the journal logs to keep only 4 weeks of logs
+read -p "Press Y to continue, or N to exit: " input
+if [[ ! $input =~ ^[Yy]$ ]]; then
+    echo "Exiting..."
+    exit 0
+fi
+
+# Vacuum the journal logs to keep only 1 week of logs
 echo "Vacuuming journal logs..."
-journalctl --vacuum-time=4weeks
+journalctl --vacuum-time=1week
+
+read -p "Press Y to continue, or N to exit: " input
+if [[ ! $input =~ ^[Yy]$ ]]; then
+    echo "Exiting..."
+    exit 0
+fi
 
 # Remove all cached versions of uninstalled packages
 echo "Removing cached versions of uninstalled packages..."
-paccache -ruk0
+echo "$1" | paccache -ruk0
 
-# Remove unused packages (orphans)
+read -p "Press Y to continue, or N to exit: " input
+if [[ ! $input =~ ^[Yy]$ ]]; then
+    echo "Exiting..."
+    exit 0
+fi
+
+# Remove Orphans
 echo "Removing orphaned packages..."
 ORPHANS=$(pacman -Qdtq)
 if [ -n "$ORPHANS" ]; then
-  sudo pacman -Rns $ORPHANS
+  echo "$1" | sudo pacman -Rns $ORPHANS
 else
   echo "No orphaned packages to remove."
 fi
